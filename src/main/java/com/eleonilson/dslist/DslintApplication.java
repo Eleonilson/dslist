511938cddcc4504a3dffd8eb95a40a50fbc28dca
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-11-11 22:12:42 -0300

    properties, Game entity, ORM

---
+++ src/main/java/com/eleonilson/dslist/DslintApplication.java
@@ -0,0 +1,13 @@
+package com.eleonilson.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslintApplication.class, args);
+	}
+
+}
