2c1a33b9dd453ca1ef4f940cb6032460ffe64924
Eleonilson <eleonilson.r.l@gmail.com.br>
2024-11-11 22:24:48 -0300

    Game seed

--- src/main/java/com/eleonilson/dslist/entities/Game.java
+++ src/main/java/com/eleonilson/dslist/entities/Game.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,19 +22,19 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private double score;
+	private Double score;
 	private String imgUrl;
 	
-	@Column(columnDefinition = "TEXTE")
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	
-	@Column(columnDefinition = "TEXTE")
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Game() {
-	}
-
-	public Game(Long id, String title, Integer year, String genre, String platforms, double score, String imgUrl,
+    public Game() {
+    }
+    
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
@@ -44,7 +44,7 @@ public class Game {
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+		this.longDescription = longDescription;		
 	}
 
 	public Long getId() {
@@ -87,11 +87,11 @@ public class Game {
 		this.platforms = platforms;
 	}
 
-	public double getScore() {
+	public Double getScore() {
 		return score;
 	}
 
-	public void setScore(double score) {
+	public void setScore(Double score) {
 		this.score = score;
 	}
 
@@ -135,9 +135,4 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
 }
